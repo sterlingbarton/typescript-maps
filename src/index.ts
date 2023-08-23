9ac6ae2886a97a6e775df5a58347427e19f166e1
Sterling <sterlingbarton81@gmail.com>
2023-08-23 14:37:28 -0600

    established map with google map api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { User } from './User'
 import { Company } from './Company'
+import { CustomMap } from './CustomMap'
 
 const user = new User()
 
 const company = new Company()
 
-console.log(user)
-console.log(company)
+const customMap = new CustomMap('map');
+
+// console.log(user)
+// console.log(company)
+
