81e3ccae0574494f6b727994d56b5a3a6b5eb1ba
Sterling <sterlingbarton81@gmail.com>
2023-08-23 15:12:07 -0600

    generated markers for user and company

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import { User } from './User'
 import { Company } from './Company'
 import { CustomMap } from './CustomMap'
 
-const user = new User()
+const user = new User();
 
-const company = new Company()
+const company = new Company();
 
 const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
 
 // console.log(user)
 // console.log(company)
